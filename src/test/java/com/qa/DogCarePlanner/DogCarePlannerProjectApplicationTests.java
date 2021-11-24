6cbe55a1a561b6fd4c555ddde016c368e3854229
Hannah <hannah.snaith1@gmail.com>
2021-11-24 15:40:22 +0000

    first commit

---
+++ src/test/java/com/qa/DogCarePlanner/DogCarePlannerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.DogCarePlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogCarePlannerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
